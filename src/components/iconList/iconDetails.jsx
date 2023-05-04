1c89fa1ae17b93be2179ee9ed63148e015c19420
mhmddev <mabrahimibagha@gmail.com>
2023-05-04 20:37:25 +0330

    feat: added size icon

--- src/components/iconList/iconDetails.jsx
+++ src/components/iconList/iconDetails.jsx
@@ -6,9 +6,11 @@ import CustomCopyToClipboard from "@/helpers/copy-to-clipboard";
 
 // define states
 const setColorIcon = 'SET_COLOR_ICON'
+const setSizeIcon = 'SET_SIZE_ICON'
 
 const initialState = {
-    color_icon: '#ffffff'
+    color_icon: '#ffffff',
+    size_icon: 'w-44 h-44',
 }
 
 const reduce = (state, action) => {
@@ -16,10 +18,21 @@ const reduce = (state, action) => {
     switch (action.type) {
         case setColorIcon:
             return {...state, color_icon: payload}
+        case setSizeIcon:
+            return {...state, size_icon: payload}
         default:
             return state
     }
 }
+
+const iconSizes = [
+    {title: 'default size', size: 'w-44 h-44'},
+    {title: 'xs', size: 'w-15 h-15'},
+    {title: 'md', size: 'w-24 h-24'},
+    {title: 'lg', size: 'w-44 h-44'},
+    {title: 'xl', size: 'w-52 h-52'},
+    {title: '2xl', size: 'w-60 h-60'},
+]
 
 const ShowDetails = ({showDetail, setShowDetail, dataIcons}) => {
     const [state, dispatch] = useReducer(reduce, initialState)
@@ -28,13 +41,14 @@ const ShowDetails = ({showDetail, setShowDetail, dataIcons}) => {
 
     const colorIcon = (e) => handleDispatch(setColorIcon, e.target.value)
 
+    const sizeIcon = (e) => handleDispatch(setSizeIcon, e.target.value)
+
     return (
         <>
             <div
                 className={`${styles.icon_modal} transition-all duration-500 ${showDetail ? 'visible opacity-100 z-[60]' : 'invisible opacity-0 z-0'}`}>
                 <div
                     className="absolute bg-secondary rounded-md h-full w-full sm:h-4/5 sm:w-11/12 md:w-4/5 lg:h-5/6 lg:w-3/4 px-7 py-6 m-20">
-                    {/* header */}
                     <div className="flex justify-between items-center">
                         {/* icon name */}
                         <CustomCopyToClipboard text={dataIcons.name}>
@@ -48,17 +62,27 @@ const ShowDetails = ({showDetail, setShowDetail, dataIcons}) => {
                             <RemoveThin className="w-10 h-10"/>
                         </button>
                     </div>
+
                     <div className="grid grid-cols-1 lg:grid-cols-2 mt-10 gap-10">
                         {/* icon preview */}
-                        <p className="grid place-items-center border py-10 rounded-xl">
-                            <dataIcons.component className="w-44 h-44" color={state.color_icon}/>
+                        <p className="grid place-items-center border w-full h-64 rounded-xl">
+                            <dataIcons.component className={state.size_icon} color={state.color_icon}/>
                         </p>
                         {/* tools */}
-                        <div className="grid grid-cols-1 lg:grid-cols-2">
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                             {/* color */}
                             <div className="w-full rounded-md bg-secondary-dark outline-none p-0.5 h-12">
                                 <input type="color"
-                                       className="w-full rounded-md p-1 bg-transparent h-full border border-solid" onChange={colorIcon} value={state.color_icon}/>
+                                       className="w-full rounded-md p-1 bg-transparent h-full border border-solid"
+                                       onChange={colorIcon} value={state.color_icon}/>
+                            </div>
+                            {/* size */}
+                            <div className="w-full rounded-md bg-secondary-dark outline-none p-0.5 h-12">
+                                <select onChange={sizeIcon} className="w-full rounded-md p-1 bg-transparent h-full border border-solid">
+                                    {iconSizes.map(({size, title}, index) => (
+                                        <option value={size} key={index}>{title}</option>
+                                    ))}
+                                </select>
                             </div>
                         </div>
                     </div>
